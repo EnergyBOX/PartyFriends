ab60a8e703ec3e263cabbd31729d6b1a1863a8ba
User <user@mail.net>
2023-11-07 19:37:29 +0200

    Goods Model was extended

--- PartyFriends/GoodsModel.swift
+++ PartyFriends/GoodsModel.swift
@@ -8,7 +8,28 @@
 import Foundation
 import SwiftUI
 
-struct GoodsModel: Identifiable {
-    var id: String = UUID().uuidString
-    var goodsName: String
+struct GoodsModel: Identifiable, Codable {
+    var id = UUID()
+    let goodsName: String
+}
+
+class GoodsList: ObservableObject {
+    var goodsArray = [GoodsModel]()  {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(goodsArray) {
+                UserDefaults.standard.set(encoded, forKey: "GoodsArray")
+            }
+        }
+    }
+    
+    init() {
+        if let savedGoodsArray = UserDefaults.standard.data(forKey: "GoodsArray") {
+            if let decodedGoodsArray = try? JSONDecoder().decode([GoodsModel].self, from: savedGoodsArray) {
+                goodsArray = decodedGoodsArray
+                return
+            }
+        }
+
+        goodsArray = []
+    }
 }
