f6b270d7b980e09ef356fb8a3c8988d46dea5916
User <user@mail.net>
2023-11-07 17:20:07 +0200

    little bit "JSON coding" from provide lesson https://www.hackingwithswift.com/books/ios-swiftui/making-changes-permanent-with-userdefaults

--- PartyFriends/PersonModel.swift
+++ PartyFriends/PersonModel.swift
@@ -1,16 +1,24 @@
 import Foundation
 import SwiftUI
 
-struct PersonModel: Identifiable {
-    let id = UUID()
-    var name: String
-    var surName: String
-    var financialCondition: Double //it must use computed properties
+struct PersonModel: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let surName: String
+    let financialCondition: Double //it must use computed properties
     //maybe there will be more properties here ...
 }
 
 class PersonList: ObservableObject {
-    @Published var personArray = [PersonModel]()
+    @Published var personArray = [PersonModel]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(personArray) {
+                UserDefaults.standard.set(encoded, forKey: "PersonArray")
+            }
+        }
+    }
+    
+    
     
     func movePerson(from source: IndexSet, to destination: Int) {
         // Implement the logic to move a person
@@ -21,6 +29,17 @@ class PersonList: ObservableObject {
     func deletePerson(index: IndexSet) {
         personArray.remove(atOffsets: index)
         print("// Implement the logic to delete a person")
+    }
+    
+    init() {
+        if let savedPersonArray = UserDefaults.standard.data(forKey: "PersonArray") {
+            if let decodedPersonArray = try? JSONDecoder().decode([PersonModel].self, from: savedPersonArray) {
+                personArray = decodedPersonArray
+                return
+            }
+        }
+
+        personArray = []
     }
 }
 
